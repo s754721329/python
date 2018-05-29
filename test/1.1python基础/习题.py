c8f85998bc1425c54096096cb68bfccbbf79602d
szy <754721329@qq.com>
2018-05-29 17:17:45 +0800

    5-29

---
+++ test/1.1python基础/习题.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+a = True
+b = not a
+def nop():
+    pass
+
+print( b )
+print( a and b )
+print( a or b )
---
+++ test/1.2函数/函数的参数.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+def school( name, age, addr='北京'):
+    print('name=',name)
+    print('age=',age)
+    print('addr=',addr)
+
+school('宋志永','25')
